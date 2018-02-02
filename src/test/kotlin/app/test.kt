cdea4a20010082f59abb02de9e04a63564593f1f
Christian Linne <christian.linne@flavia-it.de>
2018-02-02 12:42:56 +0100

    Use Spek for testing stuff ...

--- src/test/kotlin/app/test.kt
+++ src/test/kotlin/app/test.kt
@@ -1,10 +1,17 @@
 package app
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
 import kotlin.test.assertEquals
-import org.junit.Test
 
-class TestSource {
-    @Test fun f() {
-        assertEquals("Hello, world!", getGreeting())
+
+object TestSpec: Spek({
+    given("a simple starter testcase") {
+        on("addition") {
+            it("should be obvious a number equals itself") {
+                assertEquals(1, 1)
+            }
+        }
     }
-}
+})
+
