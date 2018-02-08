b6eef97a6f87d4e9ba1aab8bb388440f77a9412b
Christian Linne <christian.linne@flavia-it.de>
2018-02-08 11:09:43 +0100

    Simple Java POJO interop example; include dependency upgrade info task for gradle

--- src/main/kotlin/app/main.kt
+++ src/main/kotlin/app/main.kt
@@ -1,13 +1,15 @@
 package app
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
-
-    return words.joinToString(separator = " ")
+/**
+ * Testing some java POJO interop...
+ */
+fun javaInteropPOJO() {
+    val user = User("Urql Random") // Look 'ma, no `new` required!
+    println("""Hello ${user.name}! Your ID is ${user.id}.""") // Look 'ma, no `get` required!
+    user.name = user.name.split(" ").last() // Look 'ma, no `set` required!
+    println("""Username was changed to '${user.name}'.""")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    javaInteropPOJO()
 }
